abc947e89624e356b927a9b48d49bb846201acfa
Ahmad Farid Zulkarnain <148880065+KnightGame@users.noreply.github.com>
2026-01-06 22:30:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -301,7 +301,7 @@ def download_file_route(download_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
     
     print("=" * 70)
     print("  Universal Video Downloader - Web Version")
